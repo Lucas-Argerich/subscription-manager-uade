e3d97020bd4cb959b658577b57a8a6657a5b6018
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-10-02 12:56:54 -0300

    uso de react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import { app } from './firebase/app'
+import { createBrowserRouter, Link,  Navigate,  Outlet, RouterProvider } from 'react-router-dom'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
 
-console.log(app.name)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="auth/login">Login</Link>
+        <Link to="auth/register">Register</Link>
+      </div>
+    )
+  },
+  {
+    path: 'auth',
+    element: <Outlet />,
+    children: [
+      { index: true, element: <Navigate to="/auth/login" replace /> },
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      }
+    ]
+  }
+])
 
 export default function App() {
-  return <></>
+  return <RouterProvider router={router} />
 }
