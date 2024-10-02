4d2bbe0b15f73ba5250f1ecc09c6f320f13e128f
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-10-02 14:30:43 -0300

    lógica de login

--- src/pages/auth/Login/index.tsx
+++ src/pages/auth/Login/index.tsx
@@ -1,5 +1,35 @@
+import { useState } from 'react'
+import { auth } from '../../../firebase/app'
+import { signInWithEmailAndPassword, User } from 'firebase/auth'
+
 export default function Login() {
+  const [user, setUser] = useState<User>()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user)
+      })
+      .catch((err) => setError(err.message))
+  }
+
   return (
-    <div>Login</div>
+    <main>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleLogin}>Login</button>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </form>
+      {user && (
+        <>
+          <h3>Current User:</h3>
+          <p>{JSON.stringify(user)}</p>
+        </>
+      )}
+    </main>
   )
 }
