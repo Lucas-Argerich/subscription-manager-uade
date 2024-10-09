72c8cb9e1e8be84b306bf101ad448cf493bc44fb
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-10-08 22:40:32 -0300

    fix dependencias, colores y uso de babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
+import babel from 'vite-plugin-babel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    babel({
+      babelConfig: {
+        plugins: ['@babel/plugin-transform-runtime'],
+        presets: [['@babel/preset-react', { runtime: 'automatic' }]]
+      }
+    }),
+    react()
+  ],
   root: '.',
   resolve: {
     alias: {
@@ -13,7 +22,7 @@ export default defineConfig({
       '@examples': path.resolve(__dirname, 'src/examples'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@layouts': path.resolve(__dirname, 'src/layouts'),
-      '@firebase': path.resolve(__dirname, 'src/firebase'),
+      '@firebase': path.resolve(__dirname, 'src/firebase')
     }
   }
 })
