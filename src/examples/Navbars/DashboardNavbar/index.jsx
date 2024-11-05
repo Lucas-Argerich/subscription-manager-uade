d2980933226003e9d735c31f0f678cb7a0b1d0c1
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-11-05 15:31:33 -0300

    navbar sacar ui no usada

--- src/examples/Navbars/DashboardNavbar/index.jsx
+++ src/examples/Navbars/DashboardNavbar/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 
 // react-router components
-import { useLocation, Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 // prop-types is a library for typechecking of props.
 import PropTypes from 'prop-types'
@@ -9,13 +9,11 @@ import PropTypes from 'prop-types'
 // @material-ui core components
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import Icon from '@mui/material/Icon'
 
 // Material Dashboard 2 React components
 import MDBox from '~components/MDBox'
-import MDInput from '~components/MDInput'
 
 // Material Dashboard 2 React example components
 import Breadcrumbs from '~examples/Breadcrumbs'
@@ -26,22 +24,19 @@ import {
   navbar,
   navbarContainer,
   navbarRow,
-  navbarIconButton,
-  navbarMobileMenu,
 } from '~examples/Navbars/DashboardNavbar/styles'
 
 // Material Dashboard 2 React context
 import {
   useMaterialUIController,
   setTransparentNavbar,
-  setMiniSidenav,
-  setOpenConfigurator,
 } from '~/context'
+import MDButton from '~/components/MDButton'
 
 function DashboardNavbar({ absolute, light, isMini }) {
   const [navbarType, setNavbarType] = useState()
   const [controller, dispatch] = useMaterialUIController()
-  const { miniSidenav, transparentNavbar, fixedNavbar, openConfigurator, darkMode } = controller
+  const { transparentNavbar, fixedNavbar, darkMode } = controller
   const [openMenu, setOpenMenu] = useState(false)
   const route = useLocation().pathname.split('/').slice(1)
 
@@ -71,9 +66,6 @@ function DashboardNavbar({ absolute, light, isMini }) {
     return () => window.removeEventListener('scroll', handleTransparentNavbar)
   }, [dispatch, fixedNavbar])
 
-  const handleMiniSidenav = () => setMiniSidenav(dispatch, !miniSidenav)
-  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator)
-  const handleOpenMenu = (event) => setOpenMenu(event.currentTarget)
   const handleCloseMenu = () => setOpenMenu(false)
 
   // Render the notifications menu
@@ -95,19 +87,6 @@ function DashboardNavbar({ absolute, light, isMini }) {
     </Menu>
   )
 
-  // Styles for the navbar icons
-  const iconsStyle = ({ palette: { dark, white, text }, functions: { rgba } }) => ({
-    color: () => {
-      let colorValue = light || darkMode ? white.main : dark.main
-
-      if (transparentNavbar && !light) {
-        colorValue = darkMode ? rgba(text.main, 0.6) : text.main
-      }
-
-      return colorValue
-    },
-  })
-
   return (
     <AppBar
       position={absolute ? 'absolute' : navbarType}
@@ -120,47 +99,8 @@ function DashboardNavbar({ absolute, light, isMini }) {
         </MDBox>
         {isMini ? null : (
           <MDBox sx={(theme) => navbarRow(theme, { isMini })}>
-            <MDBox pr={1}>
-              <MDInput label="Search here" />
-            </MDBox>
             <MDBox color={light ? 'white' : 'inherit'}>
-              <Link to="/authentication/sign-in/basic">
-                <IconButton sx={navbarIconButton} size="small" disableRipple>
-                  <Icon sx={iconsStyle}>account_circle</Icon>
-                </IconButton>
-              </Link>
-              <IconButton
-                size="small"
-                disableRipple
-                color="inherit"
-                sx={navbarMobileMenu}
-                onClick={handleMiniSidenav}
-              >
-                <Icon sx={iconsStyle} fontSize="medium">
-                  {miniSidenav ? 'menu_open' : 'menu'}
-                </Icon>
-              </IconButton>
-              <IconButton
-                size="small"
-                disableRipple
-                color="inherit"
-                sx={navbarIconButton}
-                onClick={handleConfiguratorOpen}
-              >
-                <Icon sx={iconsStyle}>settings</Icon>
-              </IconButton>
-              <IconButton
-                size="small"
-                disableRipple
-                color="inherit"
-                sx={navbarIconButton}
-                aria-controls="notification-menu"
-                aria-haspopup="true"
-                variant="contained"
-                onClick={handleOpenMenu}
-              >
-                <Icon sx={iconsStyle}>notifications</Icon>
-              </IconButton>
+              <MDButton color="info">INSTALAR EXTENSIÓN</MDButton>
               {renderMenu()}
             </MDBox>
           </MDBox>
