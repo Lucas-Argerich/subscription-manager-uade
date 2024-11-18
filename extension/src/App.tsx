e5b22e2db97ca9ee42324bac404f6cf81f7d305f
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-11-18 18:45:00 -0300

    Logos y instalacion de types-chrome

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,10 +1,10 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <main className="w-56">
-      <div className="flex">
-        <h1>Subtrack</h1>
-        <p>Login</p>
-      </div>
+    <main className="w-96 min-h-96 border border-sky-500 bg-background">
+      <Header />
+  
     </main>
   )
 }
---
+++ extension/src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header className="px-5 py-3 flex justify-between items-center">
+      <img src="logo.png" width="200px" />
+    </header>
+  )
+}
