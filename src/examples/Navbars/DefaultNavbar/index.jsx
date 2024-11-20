c5f74d818be57f97aaa4213532c10acd728faef4
Lucas Argerich <lucasargerichcoder@gmail.com>
2024-11-20 19:15:28 -0300

    nombres de link en nav

--- src/examples/Navbars/DefaultNavbar/index.jsx
+++ src/examples/Navbars/DefaultNavbar/index.jsx
@@ -100,13 +100,13 @@ function DefaultNavbar({ transparent, light }) {
         <MDBox color="inherit" display={{ xs: 'none', lg: 'flex' }} m={0} p={0}>
           <DefaultNavbarLink
             icon="account_circle"
-            name="sign up"
+            name="Registrarte"
             route="/authentication/sign-up"
             light={light}
           />
           <DefaultNavbarLink
             icon="key"
-            name="sign in"
+            name="Iniciar Sesión"
             route="/authentication/sign-in"
             light={light}
           />
